3e92908427840e1abbf1cc94233614ca7311f7bd
TStorm <techstormng@gmail.com>
2023-07-26 11:42:41 +0100

    Implement button  transition, Reverse border transition

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; //skip if its not a transform
+    e.target.classList.remove('playing');
+}
+
+
+
 window.addEventListener('keydown', function(e){
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`div[data-key="${e.code}"]`);
+
     if(!audio) return; // stop function from running if audio element attached to key
+
+    key.classList.add('playing');   
     audio.currentTime = 0; // auto-rewind audio
     audio.play();
+
+    
 });
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
