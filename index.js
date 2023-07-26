26fb2a81ad516ba07c61a5c8b424c3a2b308a58f
TStorm <techstormng@gmail.com>
2023-07-26 11:59:13 +0100

    clean up code

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-function removeTransition(e){
-    if (e.propertyName !== 'transform') return; //skip if its not a transform
-    e.target.classList.remove('playing');
-}
-
-
-
-window.addEventListener('keydown', function(e){
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
     const key = document.querySelector(`div[data-key="${e.code}"]`);
 
@@ -13,12 +6,16 @@ window.addEventListener('keydown', function(e){
 
     key.classList.add('playing');   
     audio.currentTime = 0; // auto-rewind audio
-    audio.play();
+    audio.play();    
+}
 
-    
-});
+function removeTransition(e){
+    if (e.propertyName !== 'transform') return; //skip if its not a transform
+    e.target.classList.remove('playing');
+}
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
+window.addEventListener('keydown', playSound);
 
